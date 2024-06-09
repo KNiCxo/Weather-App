4be6e8f46d75c6b87f1ac92de69eb51212db12e1
KNiC.xo <ntcaliwag@hotmail.com>
2024-06-09 00:26:18 -0700

    ListView can now make API calls and display the results when using the search bar * Clicking on the magnifying glass will now search for city results based on what was input

--- src/list-view/Search.jsx
+++ src/list-view/Search.jsx
@@ -1,43 +1,74 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 // Search bar component for ListView
 function Search(props) {
-  const [locationList, setLocationList] = useState(null);
+  // Store a reference to the search bar input field
+  const inputRef = useRef();
 
-  const getLocations = async (userInput) => {
-    /* 
-    const geocodingResponse = await fetch(`
-    http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=6c873417c4c8208022b5eae05b92905a`);
-    const geocodingJSON = await geocodingResponse.json();
-    */
-   
-    setLocationList(true);
-    console.log('test');
+  // Store the list of cities that is fetched from the Geocoding API
+  const [locationList, setLocationList] = useState();
 
+  // Gets the locations from the Geocoding API that match the user input and stores them in locationList if possible
+  const getLocations = async () => {
+    // Calls API only if there is user input
+    if (inputRef.current.value) {
+      // Use user input to make API call
+      const geocodingResponse = await fetch(`
+      http://api.openweathermap.org/geo/1.0/direct?q=${inputRef.current.value}&limit=5&appid=6c873417c4c8208022b5eae05b92905a`);
+      const geocodingJSON = await geocodingResponse.json();
+  
+      // Create temporary array
+      const cityArray = []
+
+      // Go through results and insert them into temp array if necessary
+      geocodingJSON.forEach(city => {
+        // If array is empty, push first object into it
+        if (cityArray.length === 0) {
+          cityArray.push({name: city.name, state: city.state ? `${city.state},` : '', country: city.country});
+        } else {
+          // Iterate through temp array and skip result if it already exists
+          for (let i = 0; i < cityArray.length; i++) {
+            if (`${city.state},` == cityArray[i].state) {
+              return;
+            }
+          }
+          // Else, push into array
+          cityArray.push({name: city.name, state: city.state ? `${city.state},` : '', country: city.country});
+        }
+      });
+
+      // Set locationList value to that of the temp array
+      setLocationList(cityArray);
+    }
   };
   
-
   return(
     <>
+      {/* Container for search bar, magnifying glass image, and search results */}
       <div className='search'>
+        {/* Get access to input field using ref */}
         <div className='search-bar'>
-          <input type="text" placeholder='Search City'/>
+          <input ref={inputRef} type="text" placeholder='Search City'/>
           <img className='search-icon' src="search.png" alt="" onClick={getLocations}/>
         </div>
-        
-        <div className='search-results'>
-          {locationList && (
+
+        {/* Displays array if it exist s*/}
+        {locationList && (
+          <div className='search-results'>
             <ul>
-              <li>Chula Vista, US<span>Add</span></li>
-              <li>Chula Vista, MX <span>Add</span></li>
-              <li>Chula Vista, MX<span>Add</span></li>
-              <li>entry4 <span>Add</span></li>
-              <li>entry5 <span>Add</span></li>
+              {/* Loop through to display all results */}
+              {locationList.map((city, index) => (
+                <li key={index}>
+                  {/* Displays city name, state, and country, along with an "Add" button */}
+                  {city.name}, {city.state} {city.country} <span className='add-button'>Add</span>
+                </li>
+              ))}
             </ul>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </>
+    
   );
 }
 
